1f8b47e68dd1be594ec129fca872e0aed0f23b3d
tonychoi-fixel <54478101+tonychoi-fixel@users.noreply.github.com>
2022-01-10 09:53:22 -0600

    Update __manifest__.py
    license

--- receiving/__manifest__.py
+++ receiving/__manifest__.py
@@ -12,6 +12,7 @@
 
     'author': "MobleSquare",
     'website': "http://www.yourcompany.com",
+    'license': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
